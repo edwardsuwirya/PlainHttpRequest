ad4a8ef8b2c697e580b76773684be66e2b66e346
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-07-06 14:40:20 +0700

    Using coroutine

--- app/src/main/java/com/enigmacamp/myplainhttprequest/presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/enigmacamp/myplainhttprequest/presentation/MainActivityViewModel.kt
@@ -1,14 +1,20 @@
 package com.enigmacamp.myplainhttprequest.presentation
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.enigmacamp.myapplication.data.remote.responses.ImageResponse
 import com.enigmacamp.myplainhttprequest.data.repository.PlainRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
 class MainActivityViewModel(private val plainRepository: PlainRepository) : ViewModel() {
-
+    private val scope = CoroutineScope(context = Dispatchers.IO)
     private var _imageResponseLiveData = MutableLiveData<ImageResponse>()
     val imageResponseLiveData: LiveData<ImageResponse>
         get() {
@@ -16,10 +22,26 @@ class MainActivityViewModel(private val plainRepository: PlainRepository) : View
         }
 
     fun getImageList(searchName: String) {
-        thread(start = true) {
+//        thread(start = true) {
+//            val result = plainRepository.getImage(imageName = searchName)
+//            _imageResponseLiveData.postValue(result)
+//        }
+//
+//        Menggunakan lifecycle viewmodel KTX
+//        viewModelScope.launch(context = Dispatchers.IO) {
+//            val result = plainRepository.getImage(imageName = searchName)
+//            _imageResponseLiveData.postValue(result)
+//        }
+        scope.launch {
             val result = plainRepository.getImage(imageName = searchName)
             _imageResponseLiveData.postValue(result)
         }
 
+    }
+
+//    Tidak perlu method onCleared kalo menggunakn viewmodel KTX
+    override fun onCleared() {
+        Log.d("MainActivityViewModel", "onCleared: ")
+        scope.cancel()
     }
 }
